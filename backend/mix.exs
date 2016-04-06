9c1927f4aa8efbb6780a80bea36f20fd3f0ca701
Sean Callan <seancallan@gmail.com>
2016-04-06 15:39:34 -0700

    Add Ueberauth and Guardian

--- backend/mix.exs
+++ backend/mix.exs
@@ -16,11 +16,11 @@ defmodule Firestorm.Mixfile do
   # Configuration for the OTP application.
   #
   # Type `mix help compile.app` for more information.
-  def application do
-    [mod: {Firestorm, []},
-     applications: [:phoenix, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex, :comeonin]]
-  end
+def application do
+  [mod: {Firestorm, []},
+   applications: [:phoenix, :phoenix_html, :cowboy, :logger, :gettext,
+                  :phoenix_ecto, :postgrex, :comeonin, :ueberauth, :ueberauth_identity]]
+end
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
@@ -37,7 +37,10 @@ defmodule Firestorm.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
      {:cowboy, "~> 1.0"},
-     {:comeonin, "~> 2.3"}]
+     {:comeonin, "~> 2.3"},
+     {:ueberauth, "~> 0.2"},
+     {:ueberauth_identity, "~> 0.2"},
+     {:guardian, "~> 0.10"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
