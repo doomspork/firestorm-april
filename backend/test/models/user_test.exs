1374c8cb8d52a60a68af44ffe574545e141628c4
Sean Callan <seancallan@gmail.com>
2016-04-05 22:16:28 -0700

    Updated User changesets

--- backend/test/models/user_test.exs
+++ backend/test/models/user_test.exs
@@ -3,16 +3,16 @@ defmodule Firestorm.UserTest do
 
   alias Firestorm.User
 
-  @valid_attrs %{email: "some content", password: "some content", username: "some content"}
+  @valid_attrs %{email: "test@example.com", password: "some content", username: "test"}
   @invalid_attrs %{}
 
-  test "changeset with valid attributes" do
-    changeset = User.changeset(%User{}, @valid_attrs)
+  test "registration_changeset with valid attributes" do
+    changeset = User.registration_changeset(%User{}, @valid_attrs)
     assert changeset.valid?
   end
 
-  test "changeset with invalid attributes" do
-    changeset = User.changeset(%User{}, @invalid_attrs)
+  test "registration_changeset with invalid attributes" do
+    changeset = User.registration_changeset(%User{}, @invalid_attrs)
     refute changeset.valid?
   end
 end
--- backend/web/models/user.ex
+++ backend/web/models/user.ex
@@ -10,17 +10,27 @@ defmodule Firestorm.User do
     timestamps
   end
 
-  @required_fields ~w(username email encrypted_password)
-  @optional_fields ~w()
-
-  @doc """
-  Creates a changeset based on the `model` and `params`.
-
-  If no params are provided, an invalid changeset is returned
-  with no validation performed.
-  """
   def changeset(model, params \\ :empty) do
     model
-    |> cast(params, @required_fields, @optional_fields)
+    |> cast(params, ~w(username email), [])
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:username, min: 1, max: 20)
+  end
+
+  def registration_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, ~w(username email password), [])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_encrypted_pw
+  end
+
+  defp put_encrypted_pw(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :encrypted_password, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ ->
+        changeset
+    end
   end
 end
